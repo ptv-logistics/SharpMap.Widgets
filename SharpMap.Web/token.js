2904c37ac8b04ca387896136b45b6e2ec8893c8d
Oliver Heilig <oliver.heilig@yahoo.de>
2024-04-21 19:00:53 +0200

    Update token.js

--- SharpMap.Web/token.js
+++ SharpMap.Web/token.js
@@ -1,3 +1,3 @@
 ﻿// this is a time-limited demo token. 
 // You need your own xServer-internet token for your application!
-var token = 'D00DA08-6381-4E1E-8913-0243D445D6E0';
+var token = '1D00DA08-6381-4E1E-8913-0243D445D6E0';
