a34e4a48cb8ec1036e5bdc96614984806fb26b2d
PersonalAccessToken <oliver.heilig@yahoo.de>
2024-04-21 21:07:04 +0200

    Fix VirtualLayer

--- SharpMap.Web/VirtualLayer.js
+++ SharpMap.Web/VirtualLayer.js
@@ -2,7 +2,6 @@
  * L.VirtualLayer
  */
 L.VirtualLayer = L.Layer.extend({
-    includes: L.Mixin.Events,
     options: {
         attribution: '',
         minZoom: 0,
@@ -22,11 +21,6 @@ L.VirtualLayer = L.Layer.extend({
         this.name = name;
 
         L.setOptions(this, options);
-    },
-
-    addTo: function (map) {
-        map.addLayer(this);
-        return this;
     },
 
     onAdd: function (map) {
@@ -59,12 +53,13 @@ L.VirtualLayer = L.Layer.extend({
         if (this.hostLayer.wmsParams)
             this.hostLayer.wmsParams.layers = visibileLayers.join(',');
         else {
-            this.hostLayer._url = this.hostLayer._url.substring(0, this.hostLayer._url.indexOf("&layers="));
-            this.hostLayer._url = this.hostLayer._url + '&layers=' + visibileLayers.join(',');
+            var url = this.hostLayer._url = this.hostLayer._url.substring(0, this.hostLayer._url.indexOf("&layers="));
+            url = this.hostLayer._url + '&layers=' + visibileLayers.join(',');
+            this.hostLayer.setUrl(url);
         }
 
         if (visibileLayers.length == 0)
-            map.removeLayer(this.hostLayer);
+            map.removeLayer(this.hostLayer, false);
         else {
             map.addLayer(this.hostLayer);
             this.hostLayer.redraw();
